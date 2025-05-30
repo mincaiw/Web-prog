ce4929ed5d5c2781c36c0e072da649f7d2fbf381
thanhdvu <thuyanhdv@gmail.com>
2025-05-30 16:09:11 +0900

    del description

--- publish_db.py
+++ publish_db.py
@@ -7,16 +7,25 @@ conn = sqlite3.connect(yonsei_db_path)
 cursor = conn.cursor()
 
 cursor.execute('''
-    CREATE TABLE IF NOT EXISTS found_items (
+    CREATE TABLE IF NOT EXISTS found_items_new (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         prdt_cl_nm TEXT,
         start_ymd TEXT,
         prdt_nm TEXT,
         ubuilding TEXT,
-        description TEXT,
         image_path TEXT
-    );
+    )
 ''')
+
+cursor.execute('''
+    INSERT INTO found_items_new (id, prdt_cl_nm, start_ymd, prdt_nm, ubuilding, image_path)
+    SELECT id, prdt_cl_nm, start_ymd, prdt_nm, ubuilding, image_path
+    FROM found_items
+''')
+
+cursor.execute('DROP TABLE found_items')
+
+cursor.execute('ALTER TABLE found_items_new RENAME TO found_items')
 
 conn.commit()
 conn.close()
